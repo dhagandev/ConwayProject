af555eb4c3465757dc2ee33c6fbdc2366440fac4
dhagandev <dhagan@utexas.edu>
2019-06-17 21:52:36 -0500

    Attempted to copy Jen's code from old project

--- conway_app/urls.py
+++ conway_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('accounts/signup', views.signup, name='signup'),
     path('rules/', views.rules, name='rules'),
     path('sim/create/', views.consim_create, name='create'),
     path('sim/<int:consim_id>/', views.consim_detail, name='detail'),
--- conway_app/views.py
+++ conway_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def home(request):
@@ -16,3 +18,17 @@ def consim_detail(request):
 def user_profile(request):
 	user = User.objects.get(id=user_id)
 	return render(request, 'users/profile.html', {'user': user})
+
+def signup(request):
+	error_message = ''
+	if request.method == 'POST':
+		form = UserCreationForm(request.POST)
+	if form.is_valid():
+		user = form.save()
+		login(request, user)
+		return redirect('/')
+	else:
+		error_message = 'Invalid sign up - try again'
+	form = UserCreationForm()
+	context = {'form': form, 'error_message': error_message}
+	return render(request, 'registration/signup.html', context)
