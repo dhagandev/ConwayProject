0e77ac030eb9e9c0e3c7196db42177ad782470c2
dhagandev <dhagan@utexas.edu>
2019-06-20 13:50:36 -0500

    Began functionality for creating configs; model created and page updates based on user input values.

--- conway_app/models.py
+++ conway_app/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
 from django.contrib.auth.models import User
-import datetime
+from django.utils import timezone
 
 # Create your models here.
 class User(models.Model):
@@ -17,7 +17,7 @@ class ConwayConfig(models.Model):
     title = models.CharField(max_length=200)
     board = ArrayField(ArrayField(models.BooleanField()))
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
-    created = models.DateField('created', default=datetime.datetime.now())
+    created = models.DateField('created', default=timezone.now)
 
     def __str__(self):
         return self.title
---
+++ conway_app/static/js/createConfig.js
@@ -0,0 +1,205 @@
+function createGameZone() {
+	let gameZone = document.createElement("div");
+	gameZone.classList.add("game-zone");
+
+	let board = createBoard();
+
+	let boardBar = createBoardBar();
+
+	gameZone.append(board);
+	gameZone.append(boardBar);
+
+	return gameZone;
+}
+
+function createBoard() {
+	let board = document.createElement("div");
+	board.classList.add("board");
+
+	return board;
+}
+
+function createBoardBar() {
+	let boardBar = document.createElement("div");
+	boardBar.classList.add("board-bar");
+
+	let nextBtnLink = document.createElement("a");
+	nextBtnLink.setAttribute("href", "");
+
+	let nextBtn = document.createElement("div");
+	nextBtn.classList.add("white-btn");
+	nextBtn.classList.add("next-btn");
+	nextBtn.innerHTML = "Next";
+
+	nextBtnLink.appendChild(nextBtn);
+
+	let startBtnLink = document.createElement("a");
+	startBtnLink.setAttribute("href", "");
+
+	let startBtn = document.createElement("div");
+	startBtn.classList.add("white-btn");
+	startBtn.classList.add("start-btn");
+	startBtn.innerHTML = "Start";
+
+	startBtnLink.appendChild(startBtn);
+
+	boardBar.appendChild(nextBtnLink);
+	boardBar.appendChild(startBtnLink);
+
+	return boardBar;
+}
+
+function createEtcZone() {
+	let etcZone = document.createElement("div");
+	etcZone.classList.add("etc-zone");
+
+	let form = document.createElement("form");
+
+	let titleLabel = document.createElement("label");
+	titleLabel.classList.add("title-label")
+	titleLabel.setAttribute("for", "title-input-label");
+	titleLabel.innerHTML = "Title";
+
+	let titleInput = document.createElement("input");
+	titleInput.setAttribute("type", "text");
+	titleInput.setAttribute("id", "title-input");
+	titleInput.setAttribute("name", "title-input");
+	titleInput.setAttribute("maxlength", "200");
+
+	let saveLink = document.createElement("a");
+	saveLink.setAttribute("href", "");
+
+	let saveBtn = document.createElement("div");
+	saveBtn.classList.add("purple-btn");
+	saveBtn.classList.add("save-btn");
+	saveBtn.innerHTML = "Save";
+
+	saveLink.appendChild(saveBtn);
+	form.appendChild(titleLabel);
+	form.appendChild(titleInput);
+	form.appendChild(saveLink);
+	etcZone.appendChild(form);
+
+	return etcZone;
+}
+
+function createRulesZone() {
+	let rulesZone = document.createElement("div");
+	rulesZone.classList.add("rules-zone");
+
+	let rulesLabel = document.createElement("div");
+	rulesLabel.classList.add("rules-label");
+	rulesLabel.innerHTML = "Rules:"
+
+	let rulesDescrip = document.createElement("div");
+	rulesDescrip.classList.add("rules-descrip");
+
+	let ruleP = document.createElement("p");
+	ruleP.innerHTML = "1. Each alive cell with one or no neighbors dies, as if by solitude.";
+	rulesDescrip.appendChild(ruleP);
+
+	ruleP = document.createElement("p");
+	ruleP.innerHTML = "2. Each alive cell with four or more neighbors dies, as if by overpopulation.";
+	rulesDescrip.appendChild(ruleP);
+
+	ruleP = document.createElement("p");
+	ruleP.innerHTML = "3. Each alive cell with two or three neighbors survives.";
+	rulesDescrip.appendChild(ruleP);
+
+	ruleP = document.createElement("p");
+	ruleP.innerHTML = "4. Each empty cell with three neighbors becomes populated.";
+	rulesDescrip.appendChild(ruleP);
+
+	rulesZone.appendChild(rulesLabel);
+	rulesZone.appendChild(rulesDescrip);
+
+	return rulesZone
+}
+
+function createInfoZone() {
+	let infoZone = document.createElement("div");
+	infoZone.classList.add("info-zone");
+
+	let etc = createEtcZone();
+	let rules = createRulesZone();
+
+	infoZone.appendChild(etc);
+	infoZone.appendChild(rules);
+
+	return infoZone;
+}
+
+function createSimZone() {
+	let simSetup = document.querySelector(".sim-setup");
+	let simZoneParent = simSetup.parentNode;
+
+	let simZone = document.createElement("div");
+	simZone.classList.add("sim-zone")
+
+	let gameZone = createGameZone();
+	let infoZone = createInfoZone();
+
+	simZone.appendChild(gameZone);
+	simZone.appendChild(infoZone);
+	simZoneParent.appendChild(simZone);
+}
+
+function createInputError() {
+	let setupZone = document.querySelector(".setup-input");
+	let errZone = document.createElement("div");
+	errZone.classList.add("err-zone");
+	errZone.innerHTML = "Something went wrong! Your inputs must be between 3 and 50. Try again.";
+
+	setupZone.appendChild(errZone);
+}
+
+function validInput(value) {
+	if (isNaN(value) || value < 3 || value > 50) {
+		return false;
+	}
+	return true;
+}
+
+function createConfig() {
+	let gridLen = document.querySelector("#grid-len").value;
+	let gridWid = document.querySelector("#grid-wid").value;
+	
+	gridLen = parseInt(gridLen);
+	let lenOk = validInput(gridLen);
+	gridWid = parseInt(gridWid);
+	let widOk = validInput(gridWid);
+
+	let simZone = document.querySelector(".sim-zone");
+	let errZone = document.querySelector(".err-zone");
+	if (lenOk && widOk) {
+		if (errZone !== null) {
+			errZone.parentNode.removeChild(errZone)
+		}
+
+		if (simZone === null) {
+			createSimZone();
+		}
+		else {
+			simZone.parentNode.removeChild(simZone);
+			createSimZone();
+		}
+	}
+	else {
+		if (simZone !== null) {
+			simZone.parentNode.removeChild(simZone);
+		}
+
+		if (errZone !== null) {
+			errZone.parentNode.removeChild(errZone);
+		}
+
+		createInputError();
+	}
+}
+
+function setupApply() {
+	let applyBtn = document.querySelector(".apply-btn");
+	applyBtn.addEventListener("click", createConfig);
+}
+
+setupApply();
