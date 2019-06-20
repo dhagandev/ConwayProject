e53dcc76dda464b4ab0eb35fa08dd31f076b6a94
Jenny Martin <jennymartin@Jennys-MacBook-Pro.local>
2019-06-20 14:31:23 -0500

    styling updates and routing updates, signup bug fixed

--- conway_app/views.py
+++ conway_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
@@ -23,12 +23,12 @@ def signup(request):
 	error_message = ''
 	if request.method == 'POST':
 		form = UserCreationForm(request.POST)
-	if form.is_valid():
-		user = form.save()
-		login(request, user)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
 		return redirect('/')
 	else:
 		error_message = 'Invalid sign up - try again'
-	form = UserCreationForm()
-	context = {'form': form, 'error_message': error_message}
+		form = UserCreationForm()
+		context = {'form': form, 'error_message': error_message}
 	return render(request, 'registration/signup.html', context)
